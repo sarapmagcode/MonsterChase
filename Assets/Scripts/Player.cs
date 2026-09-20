d5945977e861cf4604ad6109108d0b7ac8fd7ee9
jason <markjasongalang3@gmail.com>
2026-09-20 10:00:43 +0800

    feat: add player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,69 +2,89 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float moveForce = 10f;
+    [SerializeField] // Make it configurable on the Unity editor
+    private float _moveForce = 10f;
 
-    [SerializeField]
-    private float jumpForce = 11f;
+    [SerializeField] // Make it configurable on the Unity editor
+    private float _jumpForce = 11f;
 
-    private float movementX;
+    private float _movementX;
 
-    private Rigidbody2D myBody;
+    private Rigidbody2D _myBody;
 
-    private SpriteRenderer sr;
+    private SpriteRenderer _sr;
 
-    private Animator anim;
-    private string WALK_ANIMATION = "Walk";
+    private Animator _anim;
+    private const string WalkAnimation = "Walk";
+
+    private bool _isGrounded = true;
+    private const string GroundTag = "Ground";
 
     private void Awake()
     {
-        myBody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
+        _myBody = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _sr = GetComponent<SpriteRenderer>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    private void Start() { }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         PlayerMoveKeyboard();
         AnimatePlayer();
+    }
 
+    /// <summary>
+    /// Not called every frame. We usually use this to perform physics calculation involving
+    /// the physics systems such as the RigidBody2D.
+    /// </summary>
+    private void FixedUpdate()
+    {
         PlayerJump();
     }
 
     void PlayerMoveKeyboard()
     {
-        movementX = Input.GetAxisRaw("Horizontal"); // Possible values: -1, 0, 1
-        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+        _movementX = Input.GetAxisRaw("Horizontal"); // Possible values: -1, 0, 1
+        transform.position += new Vector3(_movementX, 0f, 0f) * _moveForce * Time.deltaTime;
     }
 
     void AnimatePlayer()
     {
-        if (movementX > 0) // Right
+        if (_movementX > 0) // Right
         {
-            anim.SetBool(WALK_ANIMATION, true);
-            sr.flipX = false;
+            _anim.SetBool(WalkAnimation, true);
+            _sr.flipX = false;
         }
-        else if (movementX < 0) // Left
+        else if (_movementX < 0) // Left
         {
-            anim.SetBool(WALK_ANIMATION, true);
-            sr.flipX = true;
+            _anim.SetBool(WalkAnimation, true);
+            _sr.flipX = true;
         }
         else
         {
-            anim.SetBool(WALK_ANIMATION, false);
+            _anim.SetBool(WalkAnimation, false);
         }
     }
 
     void PlayerJump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            Debug.Log("Jump pressed");
+            _isGrounded = false;
+            _myBody.AddForce(new Vector2(0f, _jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GroundTag))
+        {
+            _isGrounded = true;
+            Debug.Log("On ground");
         }
     }
 }
